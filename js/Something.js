197bf5bfc33de69c6f13c61606d35c0158d18797
Zhang Xiawei <zhang_xiawei@hotmail.com>
2015-04-14 02:34:08 +0800

    a prototype

---
+++ js/Something.js
@@ -0,0 +1,16 @@
+var Something = function(itemName, seedPrice){
+	this.itemName = itemName;
+	this.seedPrice = seedPrice;
+	this.currentPrice = 999999999;
+	this.inhandQuantity = 0;
+	this.inhandCost = 0.0;
+};
+
+Something.prototype.updatePrice = function() {
+	this.currentPrice = Math.round(this.seedPrice * (Math.random() * 2 + 0.5));
+	return this.currentPrice;
+};
+
+Something.prototype.getInhandAveragePrice = function() {
+	return this.inhandQuantity > 0 ? this.inhandCost / this.inhandQuantity : 0;
+};
